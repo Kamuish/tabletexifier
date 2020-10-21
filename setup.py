3de8f3ec70af7439778add84fe43d926b3c04ac6
Kamuish <andremiguel952@gmail.com>
2020-10-21 15:58:22 +0100

    setup for pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tabletexifier", # Replace with your own username
+    version="0.1",
+    author="Kamuish",
+    author_email="andremiguel952@gmail.com",
+    description="Pretty formatted tables that can be exported to LaTeX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
