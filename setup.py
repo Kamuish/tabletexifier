06f624ad9d6d8a0471e9d92222e9f1e6bc350cf8
Kamuish <andremiguel952@gmail.com>
2020-10-21 19:10:12 +0100

    new and improved version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tabletexifier", 
-    version="0.1.2",
+    version="0.1.3",
     author="Kamuish",
     author_email="andremiguel952@gmail.com",
     description="Pretty formatted tables that can be exported to LaTeX",
