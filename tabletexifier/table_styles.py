b5838a091000ab9d8132f0f06cecf9165012efac
Kamuish <andremiguel952@gmail.com>
2020-10-23 14:13:21 +0100

    MNRAS style

--- tabletexifier/table_styles.py
+++ tabletexifier/table_styles.py
@@ -66,7 +66,6 @@ class Tlines(Style):
 
 
 class Alines(Style):
-
     def get_vline(self, col_number, fmt):
         """
         Return vline separator for a given column. Returns an array with bthe separator for the left and right
@@ -111,4 +110,53 @@ class Alines(Style):
 
     def get_TeX_header(self, header, alignement):
         alg_str =  ['|' for _ in header]
+        return self._build_header_vlines(alg_str, alignement)
+
+
+class MNRAS(Style):
+    def get_vline(self, col_number, fmt):
+        """
+        Return vline separator for a given column. Returns an array with bthe separator for the left and right
+        """
+        out = [' ','']
+        if fmt == 'string':
+            out = [' ', '']
+            if col_number == 0:
+                out = ['','']
+        elif fmt == 'LaTeX':
+            if col_number == 0:
+                out = [' ', '']
+            else:
+                out = [' & ','']
+
+        return out 
+
+    def get_hline(self, row_number, separator ,fmt):
+        """
+        Return horizontal separator for a given column. Returns an array with bthe separator for the above and below
+        """
+        out = ['', '\n']
+        if fmt == 'string':
+            if row_number == 0:
+                out = ['','\n' + separator + '\n']
+                out[0] = out[1]
+        if fmt == 'LaTeX':
+
+            if row_number == 0:
+                out = ['', r' \\ \hline']
+                out[0] = r'\hline'+'\n'
+            else:
+                out = ['', r'\\']
+        return out
+    
+    def get_intersection(self, col_number, fmt):
+        
+        if fmt == 'string':
+            separation = ['-', '-'] 
+        elif fmt == 'LaTeX':
+            separation = ['&','&']
+        return separation
+
+    def get_TeX_header(self, header, alignement):
+        alg_str =  ['' for _ in header]
         return self._build_header_vlines(alg_str, alignement)
